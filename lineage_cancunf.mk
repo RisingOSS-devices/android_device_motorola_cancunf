2292e49e5c1105580ba03815516c8eefb550bf97
not-ayan <notayan69@gmail.com>
2024-08-11 08:24:26 +0000

    uh

--- lineage_cancunf.mk
+++ lineage_cancunf.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/cancunf/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_cancunf
+PRODUCT_NAME := lineage_cancunf
 PRODUCT_DEVICE := cancunf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
@@ -27,3 +27,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=cancunf \
     BuildDesc="cancunf_g_sys-user 14 U1TD34.94-12-9-10 cc897d-c6704 release-keys" \
     BuildFingerprint=motorola/cancunf_g_sys/cancunf:14/U1TD34.94-12-9-10/cc897d-c6704:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Mtk 7020" \
+    RISING_MAINTAINER="Ayan"
+TARGET_ENABLE_BLUR := true
+RISING_OFFICIAL := true
+WITH_GMS := true
+RISING_MAINTAINER=Ayan
